b28593b7d2931dcf2767cdcc1f195591d6b8ab39
Tatjana Antonova <taanto@ttu.ee>
2020-02-16 00:32:45 +0200

    Update the Delete page

--- Homework3/Controllers/StudentsController.cs
+++ Homework3/Controllers/StudentsController.cs
@@ -129,7 +129,7 @@ namespace Homework3.Controllers
         }
 
         // GET: Students/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
         {
             if (id == null)
             {
@@ -137,10 +137,18 @@ namespace Homework3.Controllers
             }
 
             var student = await _context.Students
+                .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
             if (student == null)
             {
                 return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Try again, and if the problem persists " +
+                    "see your system administrator.";
             }
 
             return View(student);
